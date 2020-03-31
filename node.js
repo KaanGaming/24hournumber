57c7da32b69978d3a279bf8cb7b97ab8a91b17a3
KaanGaming <kaangaming.gamejolt@gmail.com>
2020-03-31 16:27:24 +0300

    Update node.js

--- node.js
+++ node.js
@@ -6,9 +6,9 @@ const options = { hostname: 'https://thing142.glitch.me',
   method: 'GET'
      }
 
-setInterval(https.request(options, res => {
+setInterval({ https.request(options, res => {
   const { statusCode } = res;
   
   if (statusCode == 200) { console.log("200 OK"); }
   else { console.log(`${statusCode} ERROR`); }
-}), 60000);
+})}, 60000);

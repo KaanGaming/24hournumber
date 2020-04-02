6c1b3cca570062f9f5b190bcb997b965502a7f90
KaanGaming <kaangaming.gamejolt@gmail.com>
2020-04-02 19:34:50 +0300

    Update node.js

--- node.js
+++ node.js
@@ -7,16 +7,11 @@ const options = { hostname: 'https://thing142.glitch.me',
      }
 
 function Request() {
-  https.request(options, res => {
+  https.request("https://thing143.glitch.me:3000/, res => {
     const statusCode = res.statusCode;
   
     if (statusCode == 200) { console.log("200 OK"); }
     else { console.log(`${statusCode} ERROR`); }
 });}
 
-setInterval({ https.request("https://thing143.glitch.me/", res => {
-  const statusCode = res.statusCode;
-  
-  if (statusCode == 200) { conaolo.log("200 OK"); }
-  else { console.warn(`${statusCode} ERROR`); }
-});}, 30000);
+setInterval(Request, 30000);
